9e2816d42cdd61b38cf818c8ed3e312c412204b5
Santhoshkumar M C <136875136+Santhoskum@users.noreply.github.com>
2023-08-26 14:40:44 +0530

    Add files via upload

---
+++ word counter/script.js
@@ -0,0 +1,7 @@
+function formDataShow(){
+    let str=document.getElementById("word").value
+    let array=str.split(" ")
+    let num=array.length
+    let result=document.getElementById("result")
+    result.innerHTML="Total words : "+num}
+
